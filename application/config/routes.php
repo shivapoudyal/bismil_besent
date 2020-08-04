41dd659e989ff7b4ffe53385fb6ff2d706ae5430
shivapoudyal <shivpodi04@gmail.com>
2020-08-04 15:53:11 +0530

    committed by shiva

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,10 +49,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'cloudUpload';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-$route['about-us'] = 'Welcome/about_us/';
-$route['contact-us'] = 'Welcome/contact_us/';
-$route['service'] = 'Welcome/service/';
-//$route['service-part'] = 'Welcome/service_part/';
+//$route['about-us'] = 'Welcome/about_us/';
+//$route['contact-us'] = 'Welcome/contact_us/';
+//$route['service'] = 'Welcome/service/';
+////$route['service-part'] = 'Welcome/service_part/';
