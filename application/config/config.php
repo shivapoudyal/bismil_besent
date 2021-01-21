2c3d05653e02a7bafb75126b535a4deac46c3a1b
shivapoudyal <shivpodi04@gmail.com>
2021-01-21 20:54:16 +0530

    shiva committed

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/apsos/';
+$config['base_url'] = 'http://localhost/bismil_soft/';
 
 /*
 |--------------------------------------------------------------------------
